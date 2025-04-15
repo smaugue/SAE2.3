6393cc1771c46e07b5f8b1b168ac2cafcdda3c9d
MDarvas <maxdarvas@gmail.com>
2025-04-15 09:06:27 +0200

    Add files via upload

---
+++ acceuil.js
@@ -0,0 +1,12 @@
+// Sélectionner l'icône du menu burger et le menu
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+
+// Ajouter un événement de clic sur l'icône burger
+hamburger.addEventListener('click', () => {
+  // Ajouter ou retirer la classe "open" pour afficher ou cacher le menu
+  menu.classList.toggle('open');
+});
+document.querySelector('.image-container').addEventListener('click', () => {
+  alert('Clique détecté partout sur le bloc');
+});
