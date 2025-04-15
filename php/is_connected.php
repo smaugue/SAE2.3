f7f8cbbc00bd5773300ad20f18383ec05e545006
Smaugue <ssmaugue@gmail.com>
2025-04-15 11:04:54 +0200

    update login + is_connectde php

--- php/is_connected.php
+++ php/is_connected.php
@@ -4,9 +4,10 @@ session_start();
 // Check if the user is connected
 if (!isset($_SESSION['user_id'])) {
     // Redirect to login.html if not connected
-    header('Location: login.html');
+    header('Location: /login.html');
     exit();
 }
+echo "mdr";
 
 // User is connected, continue with the rest of the script
 ?>
