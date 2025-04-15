6efaa32d6af5ac710eec6f4595f181c51f499a89
MDarvas <maxdarvas@gmail.com>
2025-04-15 09:49:54 +0200

    modif accueil css html js

---
+++ accueil.js
@@ -0,0 +1,9 @@
+// Sélectionner l'icône du menu burger et le menu
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+
+// Ajouter un événement de clic sur l'icône burger
+hamburger.addEventListener('click', () => {
+  // Ajouter ou retirer la classe "open" pour afficher ou cacher le menu
+  menu.classList.toggle('open');
+});
