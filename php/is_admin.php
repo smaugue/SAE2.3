c507ab21d8e9aed92dd9b9cf7d15e18ed05dab4b
Smaugue <ssmaugue@gmail.com>
2025-04-16 15:52:26 +0200

    Update is_admin.php

--- php/is_admin.php
+++ php/is_admin.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
+echo($_SESSION['is_admin']);
 if ($_SESSION['is_admin'] != TRUE) {
-    header("Location: php/logout.php");
-    exit();
+    echo "Vous n'avez pas accès à cette page.";
+    //header("Location: php/logout.php");
+    //exit();
 }
 ?>
