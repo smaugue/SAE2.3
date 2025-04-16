922e15e09c95bc279668663d3331e47bcbc034d4
Smaugue <ssmaugue@gmail.com>
2025-04-16 12:32:51 +0200

    try patch bug add course

--- php/get_lieux.php
+++ php/get_lieux.php
@@ -6,6 +6,8 @@ header('Content-Type: application/json');
 
 try {
     $stmt = $pdo->query("SELECT id_lieux, Nom, Ville, Type FROM Lieux");
+
+    //génère tbl associatif avec les lieux
     $lieux = $stmt->fetchAll(PDO::FETCH_ASSOC); //i wana cry help me plz
     //la on renvoi le tableau de lieux en json (pcq c le plus simple avec le js derrière)
     echo json_encode($lieux);
